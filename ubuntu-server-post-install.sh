b4dec44bd180e711fbcc1c0f161933bfc59aaebd
Damien Butt <damien.butt@norgateav.com>
2021-02-06 00:59:43 +0000

    Update ubuntu-server-post-install.sh
    Remove checking of user permissions - Unnecessary
    Remove editing of fail2ban config file - Problematic
    Remove installation of oh-my-zsh - Unnecessary

--- ubuntu-server-post-install.sh
+++ ubuntu-server-post-install.sh
@@ -13,21 +13,7 @@ sleep 2
 sudo apt install unattended-upgrades -y
 sudo dpkg-reconfigure --priority=low unattended-upgrades
 cat /etc/apt/apt.conf.d/20auto-upgrades
-
-# Check user permissions
-clear
-echo "Checking user permissions..."
-echo "Enter the username you created during the ubuntu-server installtion: "
-read username
-if sudo -l -U $username | grep -q "(ALL : ALL) ALL"; then
-
-    # Disable root login
-    clear
-    echo "Disabling root login..."
-    sleep 2
-    sudo passwd -l root
-
-fi
+sleep 2
 
 # Install QEMU guest agent
 clear
@@ -47,6 +33,7 @@ sudo ufw allow ssh
 
 sudo ufw enable
 sudo ufw status
+sleep 2
 
 # Install fail2ban
 clear
@@ -57,18 +44,5 @@ sudo apt install fail2ban -y
 
 sudo systemctl status fail2ban
 
-sudo cp /etc/fail2ban/fail2ban.{conf,local}
-sudo cp /etc/fail2ban/jail.{conf,local}
-
-sudo sed -i "s/backend = auto/backend = systemd/" /etc/fail2ban/jail.local
-
 sudo fail2ban-client status
 tail /var/log/fail2ban.log
-
-# Install ZSH and oh-my-zsh
-clear
-echo "Installing ZSH and oh-my-zsh..."
-sleep 2
-sudo apt update
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
